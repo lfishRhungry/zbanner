87eda97aa77049284770b29e62769f41fc8679d8
lfishRhungry <shineccy@aliyun.com>
2023-12-14 09:01:06 +0800

    accurate the comments

--- src/stack-tcp-core.h
+++ src/stack-tcp-core.h
@@ -15,6 +15,7 @@ struct ProtocolParserStream;
 #define TCP_SEQNO(px,i) (px[i+4]<<24|px[i+5]<<16|px[i+6]<<8|px[i+7])
 #define TCP_ACKNO(px,i) (px[i+8]<<24|px[i+9]<<16|px[i+10]<<8|px[i+11])
 #define TCP_FLAGS(px,i) (px[(i)+13])
+/* It is actually TCP_HAS_XXX instead of TCP_IS_XXX because of the calc */
 #define TCP_IS_SYNACK(px,i) ((TCP_FLAGS(px,i) & 0x12) == 0x12)
 #define TCP_IS_ACK(px,i) ((TCP_FLAGS(px,i) & 0x10) == 0x10)
 #define TCP_IS_RST(px,i) ((TCP_FLAGS(px,i) & 0x4) == 0x4)
