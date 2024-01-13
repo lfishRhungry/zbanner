921f9a7d56123b9d4c8f9e7da9f27544107d607e
lfish <shineccy@aliyun.com>
2024-01-13 15:46:12 +0800

    update info

--- src/main.c
+++ src/main.c
@@ -1527,7 +1527,7 @@ main_scan(struct Masscan *masscan)
         now = time(0);
         safe_gmtime(&x, &now);
         strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S GMT", &x);
-        LOG(0, "Starting masscan " MASSCAN_VERSION "\n(https://github.com/lfishRhungry/masscan-learn/tree/stateless) at %s\n",
+        LOG(0, "Starting zbanner " MASSCAN_VERSION "\n(https://github.com/lfishRhungry/zbanner) at %s\n",
             buffer);
 
         if (count_ports == 1 && \
