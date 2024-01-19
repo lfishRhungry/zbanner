8370941b6bc8098f1a3a8f515377ade9d06ba829
lfishRhungry <shineccy@aliyun.com>
2024-01-19 08:27:24 +0800

    improve `is_singleton`

--- src/main-conf.c
+++ src/main-conf.c
@@ -3778,31 +3778,7 @@ is_numable(const char *name) {
 static int
 is_singleton(const char *name)
 {
-    static const char *singletons[] = {
-        "echo", "echo-all", "echo-cidr", "selftest", "self-test", "regress",
-        "benchmark",
-        "system-dns", "traceroute", "version",
-        "version-light",
-        "version-all", "version-trace",
-        "osscan-limit", "osscan-guess",
-        "badsum", "reason", "open", "open-only",
-        "packet-trace", "release-memory",
-        "log-errors", "append-output", "webxml",
-        "no-stylesheet", "heartbleed", "ticketbleed",
-        "send-eth", "send-ip", "iflist",
-        "nmap", "trace-packet", "pfring", "sendq",
-        "ping", "ping-sweep", "nobacktrace", "backtrace",
-        "infinite", "nointeractive", "interactive", "status", "nostatus",
-        "read-range", "read-ranges", "readrange", "read-ranges",
-        0};
-    size_t i;
-
-    for (i=0; singletons[i]; i++) {
-        if (EQUALS(singletons[i], name))
-            return 1;
-    }
-    
-    for (i=0; config_parameters[i].name; i++) {
+    for (size_t i=0; config_parameters[i].name; i++) {
         if (EQUALS(config_parameters[i].name, name)) {
             return (config_parameters[i].flags & F_BOOL) == F_BOOL;
         } else {
