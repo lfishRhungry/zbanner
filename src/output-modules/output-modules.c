49936157f4f6a58d064c24567ab0b99888e022b3
lfishRhungry <shineccy@aliyun.com>
2024-01-25 16:16:18 +0800

    update output

--- src/output-modules/output-modules.c
+++ src/output-modules/output-modules.c
@@ -20,13 +20,24 @@ output_tmp(
     ipaddress_formatted_t ip_me_fmt = ipaddress_fmt(ip_me);
     ipaddress_formatted_t ip_them_fmt = ipaddress_fmt(ip_them);
 
-    char fmt[] = "Discovered: %-15s on port %-5u is %s because of %-8s";
-    unsigned count = fprintf(stdout, fmt, ip_them_fmt.string, port_them,
-        successed?"success":"failure",
-        classification);
+    unsigned count = 0;
+
+    if (parsed->found==FOUND_ICMP) {
+        char fmt[] = "%s on host: %-15s because of %-8s";
+        count = fprintf(stdout, fmt,
+            successed?"Success":"Failure",
+            ip_them_fmt.string,
+            classification);
+    } else {
+        char fmt[] = "%s on host: %-15s port: %-5u because of %s";
+        count = fprintf(stdout, fmt,
+            successed?"Success":"Failure",
+            ip_them_fmt.string, port_them,
+            classification);
+    }
     
     if (is_show_report) {
-        count += fprintf(stdout, " Report: [%s]", report);
+        count += fprintf(stdout, ". Report: [%s]", report);
     }
     
     
