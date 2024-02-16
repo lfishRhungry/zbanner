8b6c1746030dbca7e17e4d54e47db7598be7db0b
lfishRhungry <shineccy@aliyun.com>
2024-02-16 17:35:22 +0800

    code format

--- src/scan-modules/zbanner-scan.c
+++ src/scan-modules/zbanner-scan.c
@@ -1,12 +1,3 @@
-/**
- * Format of 32bits Init Sequence Number of SYN packet
- 0                   1                   2                   3
- 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2
- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- |      Hash for Validating      |      Hash for randomizing     |
- +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-*/
-
 #include <stdlib.h>
 
 #include "scan-modules.h"
