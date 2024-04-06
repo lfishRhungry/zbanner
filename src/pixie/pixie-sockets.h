0390f0f940e749320d4317a0b0de3d165db38b3f
lfishRhungry <shineccy@aliyun.com>
2024-04-06 23:42:13 +0800

    add useful socket header

--- src/pixie/pixie-sockets.h
+++ src/pixie/pixie-sockets.h
@@ -3,10 +3,12 @@
 #include <stddef.h>
 #if defined(WIN32)
 #include <WinSock2.h>
+#include <WS2tcpip.h>
 #else
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 typedef int SOCKET;
 #endif
 
