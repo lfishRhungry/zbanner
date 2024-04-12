f5a8bd5c9cb2d6d12eb786885e235988bdda8497
lfishRhungry <shineccy@aliyun.com>
2024-04-12 22:17:10 +0800

    format

--- src/stub/stub-pfring.h
+++ src/stub/stub-pfring.h
@@ -24,28 +24,28 @@ struct pfring_pkthdr {
   /* only filled in if PF_RING_LONG_HEADER set */
   unsigned char extended_hdr[512];
 };
-#define PF_RING_ERROR_GENERIC              -1
-#define PF_RING_ERROR_INVALID_ARGUMENT     -2
-#define PF_RING_ERROR_NO_PKT_AVAILABLE     -3
-#define PF_RING_ERROR_NO_TX_SLOT_AVAILABLE -4
-#define PF_RING_ERROR_WRONG_CONFIGURATION  -5
-#define PF_RING_ERROR_END_OF_DEMO_MODE     -6
-#define PF_RING_ERROR_NOT_SUPPORTED        -7
-#define PF_RING_ERROR_INVALID_LIB_VERSION  -8
-#define PF_RING_ERROR_UNKNOWN_ADAPTER      -9
-#define PF_RING_ERROR_NOT_ENOUGH_MEMORY   -10
-#define PF_RING_ERROR_INVALID_STATUS      -11
-#define PF_RING_DNA_SYMMETRIC_RSS    1 << 0
-#define PF_RING_REENTRANT            1 << 1
-#define PF_RING_LONG_HEADER          1 << 2
-#define PF_RING_PROMISC              1 << 3
-#define PF_RING_TIMESTAMP            1 << 4
-#define PF_RING_HW_TIMESTAMP         1 << 5
-#define PF_RING_RX_PACKET_BOUNCE     1 << 6
-#define PF_RING_DNA_FIXED_RSS_Q_0    1 << 7
-#define PF_RING_STRIP_HW_TIMESTAMP   1 << 8
-#define PF_RING_DO_NOT_PARSE         1 << 9  /* parsing already disabled in zero-copy */
-#define PF_RING_DO_NOT_TIMESTAMP     1 << 10 /* sw timestamp already disabled in zero-copy */
+#define PF_RING_ERROR_GENERIC                               -1
+#define PF_RING_ERROR_INVALID_ARGUMENT                      -2
+#define PF_RING_ERROR_NO_PKT_AVAILABLE                      -3
+#define PF_RING_ERROR_NO_TX_SLOT_AVAILABLE                  -4
+#define PF_RING_ERROR_WRONG_CONFIGURATION                   -5
+#define PF_RING_ERROR_END_OF_DEMO_MODE                      -6
+#define PF_RING_ERROR_NOT_SUPPORTED                         -7
+#define PF_RING_ERROR_INVALID_LIB_VERSION                   -8
+#define PF_RING_ERROR_UNKNOWN_ADAPTER                       -9
+#define PF_RING_ERROR_NOT_ENOUGH_MEMORY                    -10
+#define PF_RING_ERROR_INVALID_STATUS                       -11
+#define PF_RING_DNA_SYMMETRIC_RSS                      1 <<  0
+#define PF_RING_REENTRANT                              1 <<  1
+#define PF_RING_LONG_HEADER                            1 <<  2
+#define PF_RING_PROMISC                                1 <<  3
+#define PF_RING_TIMESTAMP                              1 <<  4
+#define PF_RING_HW_TIMESTAMP                           1 <<  5
+#define PF_RING_RX_PACKET_BOUNCE                       1 <<  6
+#define PF_RING_DNA_FIXED_RSS_Q_0                      1 <<  7
+#define PF_RING_STRIP_HW_TIMESTAMP                     1 <<  8
+#define PF_RING_DO_NOT_PARSE                           1 <<  9  /* parsing already disabled in zero-copy */
+#define PF_RING_DO_NOT_TIMESTAMP                       1 << 10 /* sw timestamp already disabled in zero-copy */
 
 /*
  * function prototypes
