4bf6818b2454483a64b732159b29431c16e53745
lfishRhungry <shineccy@aliyun.com>
2024-04-29 23:49:28 +0800

    fix error on Windows

--- src/pixie/pixie-threads.c
+++ src/pixie/pixie-threads.c
@@ -262,8 +262,8 @@ void pixie_set_thread_name(const char *name) {
 #pragma warning(push)
 #pragma warning(disable : 6320 6322)
 //   __try {
-      RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR),
-                     (ULONG_PTR *)&info);
+//       RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR),
+//                      (ULONG_PTR *)&info);
 //   } __except (EXCEPTION_EXECUTE_HANDLER) {
 //   }
 #pragma warning(pop)
