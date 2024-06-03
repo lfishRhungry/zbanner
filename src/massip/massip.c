071b7fa19848f2fd2af3894fdeae74b426f00aa8
sharkocha <shineccy@aliyun.com>
2024-06-03 16:20:19 +0800

    update massip protocol number

--- src/massip/massip.c
+++ src/massip/massip.c
@@ -15,7 +15,7 @@ const char *ip_proto_to_string(unsigned ip_proto)
         case IP_PROTO_ICMP:          return "ICMP";
         case IP_PROTO_IGMP:          return "IGMP";
         case IP_PROTO_GGP:           return "GGP";
-        case IP_PROTO_IP:            return "IP";
+        case IP_PROTO_IPv4:          return "IPv4";
         case IP_PROTO_TCP:           return "TCP";
         case IP_PROTO_EGP:           return "EGP";
         case IP_PROTO_IGP:           return "IGP";
@@ -25,18 +25,16 @@ const char *ip_proto_to_string(unsigned ip_proto)
         case IP_PROTO_IPv6_Frag:     return "IPv6_Frag";
         case IP_PROTO_IDRP:          return "IDRP";
         case IP_PROTO_GRE:           return "GRE";
-        case IP_PROTO_MHRP:          return "MHRP";
-        case IP_PROTO_ESP:           return "ESP";
-        case IP_PROTO_AH:            return "AH";
-        case IP_PROTO_MOBILE:        return "MOBILE";
+        case IP_PROTO_Min_IPv4:      return "Min_IPv4";
         case IP_PROTO_IPv6_ICMP:     return "IPv6_ICMP";
         case IP_PROTO_IPv6_NoNxt:    return "IPv6_NoNxt";
         case IP_PROTO_IPv6_Opts:     return "IPv6_Opts";
-        case IP_PROTO_IPIP:          return "IPIP";
+        case IP_PROTO_OSPFIGP:       return "OSPFIGP";
         case IP_PROTO_ETHERIP:       return "ETHERIP";
-        case IP_PROTO_ENCAP:         return "ENCAP";
         case IP_PROTO_L2TP:          return "L2TP";
+        case IP_PROTO_ISIS_over_IPv4:return "ISIS_over_IPv4";
         case IP_PROTO_SCTP:          return "SCTP";
+        case IP_PROTO_MPLS_in_IP:    return "MPLS_in_IP";
 
         default:
             return "Other";
--- src/massip/massip.h
+++ src/massip/massip.h
@@ -32,36 +32,34 @@ struct MassIP {
 
 
 /**
- * For present ip protocol number and diff port type.
- * Use it as unsigned type.
+ * For showing ip protocol and diffing port type.
+ * Ref: https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
 */
-#define IP_PROTO_HOPOPT            0
-#define IP_PROTO_ICMP              1
-#define IP_PROTO_IGMP              2
-#define IP_PROTO_GGP               3
-#define IP_PROTO_IP                4
-#define IP_PROTO_TCP               6
-#define IP_PROTO_EGP               8
-#define IP_PROTO_IGP               9
-#define IP_PROTO_UDP              17
-#define IP_PROTO_IPv6             41
-#define IP_PROTO_IPv6_Route       43
-#define IP_PROTO_IPv6_Frag        44
-#define IP_PROTO_IDRP             45
-#define IP_PROTO_GRE              47
-#define IP_PROTO_MHRP             48
-#define IP_PROTO_ESP              50
-#define IP_PROTO_AH               51
-#define IP_PROTO_MOBILE           55
-#define IP_PROTO_IPv6_ICMP        58
-#define IP_PROTO_IPv6_NoNxt       59
-#define IP_PROTO_IPv6_Opts        60
-#define IP_PROTO_IPIP             94
-#define IP_PROTO_ETHERIP          97
-#define IP_PROTO_ENCAP            98
-#define IP_PROTO_L2TP            115
-#define IP_PROTO_SCTP            132
-#define IP_PROTO_Other            ~0
+#define IP_PROTO_HOPOPT            0    /*IPv6 Hop-by-Hop Option*/
+#define IP_PROTO_ICMP              1    /*Internet Control Message*/
+#define IP_PROTO_IGMP              2    /*Internet Group Management*/
+#define IP_PROTO_GGP               3    /*Gateway-to-Gateway*/
+#define IP_PROTO_IPv4              4    /*IPv4 encapsulation*/
+#define IP_PROTO_TCP               6    /*Transmission Control*/
+#define IP_PROTO_EGP               8    /*Exterior Gateway Protocol*/
+#define IP_PROTO_IGP               9    /*any private interior gateway (used by Cisco for their IGRP)*/
+#define IP_PROTO_UDP              17    /*User Datagram*/
+#define IP_PROTO_IPv6             41    /*IPv6 encapsulation*/
+#define IP_PROTO_IPv6_Route       43    /*Routing Header for IPv6*/
+#define IP_PROTO_IPv6_Frag        44    /*Fragment Header for IPv6*/
+#define IP_PROTO_IDRP             45    /*Inter-Domain Routing Protocol*/
+#define IP_PROTO_GRE              47    /*Generic Routing Encapsulation*/
+#define IP_PROTO_Min_IPv4         55    /*Minimal IPv4 Encapsulation (for mobile)*/
+#define IP_PROTO_IPv6_ICMP        58    /*ICMP for IPv6*/
+#define IP_PROTO_IPv6_NoNxt       59    /*No Next Header for IPv6*/
+#define IP_PROTO_IPv6_Opts        60    /*Destination Options for IPv6*/
+#define IP_PROTO_OSPFIGP          89    /*OSPF*/
+#define IP_PROTO_ETHERIP          97    /*Ethernet-within-IP Encapsulation*/
+#define IP_PROTO_L2TP            115    /*Layer Two Tunneling Protocol*/
+#define IP_PROTO_ISIS_over_IPv4  124
+#define IP_PROTO_SCTP            132    /*Stream Control Transmission Protocol*/
+#define IP_PROTO_MPLS_in_IP      137
+#define IP_PROTO_Other           255    /*For unregisted here or unknown type*/
 
 const char *ip_proto_to_string(unsigned ip_proto);
 
