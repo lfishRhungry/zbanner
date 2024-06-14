dd7238be88b02b20bb02d05148f44b97e8bcbdea
sharkocha <shineccy@aliyun.com>
2024-06-14 20:26:02 +0800

    update lua example script comments

--- lua-probes/tcp-example.lua
+++ lua-probes/tcp-example.lua
@@ -1,8 +1,13 @@
---Configs
+-- Configs
+---@string name of the probe.
 ProbeName = "tcp-example"
-ProbeType = "tcp"         -- tcp, udp or state
-MultiMode = "null"        -- null, direct, if_open, after_handle or dynamic_next
+---@string type of the probe, could be 'tcp', 'udp' or 'state'.
+ProbeType = "tcp"
+---@string multi-probe mode, could be 'null', 'direct', 'if_open', 'after_handle' or 'dynamic_next'.
+MultiMode = "null"
+---@number multi-probe number.
 MultiNum  = 1
+---@string description of the probe.
 ProbeDesc = [[
     "This is an example lua script for tcp type probe. It sends http simple get "
     "request and identifies http service."
@@ -12,45 +17,43 @@ ProbeDesc = [[
 local hello_string = "GET / HTTP/1.0\r\n\r\n"
 
 
---- To make hello data
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @return string data of hello
+-- To make hello data.
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@return string data of hello.
 function Make_payload(ip_them, port_them, ip_me, port_me, index)
     return hello_string
 end
 
 
---- To get hello data length
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @return int length of hello data
+-- To get hello data length.
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@return number length of hello data.
 function Get_payload_length(ip_them, port_them, ip_me, port_me, index)
     return #hello_string
 end
 
 
---- To handle reponse data
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @return integer positive for starting after_handle or index +1 to set next probe in dynamic_next
--- @return boolean result if a successful response
--- @return string classification of result
--- @return string reason of classification
--- @return string report of response (empty ret value if no report)
+-- To handle reponse data.
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@param response string reponsed data.
+---@return number positive for starting after_handle or index +1 to set next probe in dynamic_next.
+---@return boolean result if a successful response.
+---@return string classification of result.
+---@return string reason of classification.
+---@return string report of response (empty string if no report).
 function Handle_response(ip_them, port_them, ip_me, port_me, index, response)
-    if #response==0 then
-        return 0, false, "no service", "timeout", ""
-    end
 
     if not string.find(response, "HTTPS") and
         (string.find(response, "HTTP")
@@ -64,17 +67,17 @@ function Handle_response(ip_them, port_them, ip_me, port_me, index, response)
 end
 
 
---- To handle reponse timeout
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @return integer positive for starting after_handle or index +1 to set next probe in dynamic_next
--- @return boolean result if a successful response
--- @return string classification of result
--- @return string reason of classification
--- @return string report of response (empty ret value if no report)
+-- To handle reponse timeout.
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@return number positive for starting after_handle or index +1 to set next probe in dynamic_next.
+---@return boolean result if a successful response.
+---@return string classification of result.
+---@return string reason of classification.
+---@return string report of response (empty ret value if no report).
 function Handle_timeout(ip_them, port_them, ip_me, port_me, index)
     return 0, false, "no service", "timeout", ""
 end
--- lua-probes/udp-example.lua
+++ lua-probes/udp-example.lua
@@ -1,8 +1,13 @@
---Configs
+-- Configs
+---@string name of the probe.
 ProbeName = "udp-example"
-ProbeType = "udp"         -- tcp, udp or state
-MultiMode = "null"        -- null, direct, if_open, after_handle or dynamic_next
+---@string type of the probe, could be 'tcp', 'udp' or 'state'.
+ProbeType = "udp"
+---@string multi-probe mode, could be 'null', 'direct', 'if_open', 'after_handle' or 'dynamic_next'.
+MultiMode = "null"
+---@number multi-probe number.
 MultiNum  = 1
+---@string description of the probe.
 ProbeDesc = [[
     "This is an example lua script for udp type probe. It sends a TXT version "
     "bind request and identifies dns service."
@@ -22,14 +27,14 @@ local dns_req ="\x50\xb6\x01\x20\x00\x01\x00\x00\x00\x00\x00\x00\x07version\x04b
 ]]
 
 
---- To make hello data
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @int cookie suggested cookie of this target
--- @return string data of hello
+-- To make hello data
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@param cookie number suggested cookie of this target
+---@return string data of hello.
 function Make_payload(ip_them, port_them, ip_me, port_me, index, cookie)
     local send_req = dns_req
     send_req = string.char(cookie & 0xFF) .. send_req:sub(2)
@@ -38,15 +43,15 @@ function Make_payload(ip_them, port_them, ip_me, port_me, index, cookie)
 end
 
 
---- To validate response
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @int cookie suggested cookie of this target
--- @response string reponsed data
--- @return boolean if response data if valid
+-- To validate response
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@param cookie number suggested cookie of this target
+---@param response string reponsed data.
+---@return boolean if response data is valid
 function Validate_response(ip_them, port_them, ip_me, port_me, index, cookie, response)
     local a = string.pack("B", cookie & 0xFF)
     if response:sub(1,1)~=a then
@@ -62,46 +67,34 @@ function Validate_response(ip_them, port_them, ip_me, port_me, index, cookie, re
 end
 
 
---- To handle reponse data
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @response string reponsed data
--- @return integer positive for starting after_handle or index +1 to set next probe in dynamic_next
--- @return boolean result if a successful response
--- @return string classification of result
--- @return string reason of classification
--- @return string report of response (empty ret value if no report)
+-- To handle reponse data
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@param response string reponsed data.
+---@return number positive for starting after_handle or index +1 to set next probe in dynamic_next.
+---@return boolean result if a successful response.
+---@return string classification of result.
+---@return string reason of classification.
+---@return string report of response (empty string if no report).
 function Handle_response(ip_them, port_them, ip_me, port_me, index, response)
-    if #response==0 then
-        return 0, false, "no service", "timeout", ""
-    end
-
-    -- if not string.find(response, "HTTPS") and
-    --     (string.find(response, "HTTP")
-    --     or string.find(response, "html")
-    --     or string.find(response, "HTML")
-    --     or string.find(response, "<h1>")) then
-    --     return 0, true, "identified", "matched", "http service"
-    -- end
-
     return 0, true, "identified", "matched", "dns"
 end
 
 
---- To handle reponse timeout
--- @string ip_them ip of target
--- @int port_them port of target
--- @string ip_me ip of us
--- @int port_me port of us
--- @int index index of expected hello probe
--- @return integer positive for starting after_handle or index +1 to set next probe in dynamic_next
--- @return boolean result if a successful response
--- @return string classification of result
--- @return string reason of classification
--- @return string report of response (empty ret value if no report)
+-- To handle reponse timeout
+---@param ip_them string ip of target.
+---@param port_them number port of target.
+---@param ip_me string ip of us.
+---@param port_me number port of us.
+---@param index number index of expected hello probe.
+---@return number positive for starting after_handle or index +1 to set next probe in dynamic_next.
+---@return boolean result if a successful response.
+---@return string classification of result.
+---@return string reason of classification.
+---@return string report of response (empty ret value if no report).
 function Handle_timeout(ip_them, port_them, ip_me, port_me, index)
     return 0, false, "no service", "timeout", ""
 end
