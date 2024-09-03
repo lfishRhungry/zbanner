bf47cc4c55147e71cb9ef147d2a9e85c874bedcf
sharkocha <shineccy@aliyun.com>
2024-09-03 16:48:09 +0800

    better script for building and cleaning

--- build.sh
+++ build.sh
@@ -1,19 +1,33 @@
-rm -rf ./build/*
+#!/bin/bash
+set -e
+set -o pipefail
+
+echo "=====Cleaning previous build and binary directories..."
 rm -rf ./build/*
 rm -rf ./bin/*
+
+mkdir -p ./build
 cd ./build
 
-if [[ "$1" = "debug" ]];then
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+if [[ "$1" == "debug" ]]; then
+    echo "=====Configuring project for Debug build..."
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
 else
-cmake ..
+    echo "=====Configuring project for Release build..."
+    cmake ..
 fi
 
-make -j8
+echo "=====Building the project..."
+make -j$(nproc)
+
 cd ..
 
-if [[ "$1" = "debug" ]];then
-cp ./build/xtate_debug ./bin/
+if [[ "$1" == "debug" ]]; then
+    echo "=====Copying debug executable to ./bin/ directory..."
+    cp ./build/xtate_debug ./bin/
 else
-cp ./build/xtate ./bin/
+    echo "=====Copying release executable to ./bin/ directory..."
+    cp ./build/xtate ./bin/
 fi
+
+echo "=====Build and copy process completed successfully."
--- clean.sh
+++ clean.sh
@@ -1,4 +1,21 @@
-echo -e "Cleaning build files of xtate..."
-rm -rf ./build/*
-rm -rf ./bin/*
-echo -e "Clean successed!"
+#!/bin/bash
+set -e
+set -o pipefail
+
+echo "=====Cleaning build and binary files of xtate..."
+
+if [ -d "./build" ]; then
+    rm -rf ./build/*
+    echo "=====Build directory cleaned."
+else
+    echo "=====Build directory does not exist, nothing to clean."
+fi
+
+if [ -d "./bin" ]; then
+    rm -rf ./bin/*
+    echo "=====Binary directory cleaned."
+else
+    echo "=====Binary directory does not exist, nothing to clean."
+fi
+
+echo "=====Clean succeeded!"
