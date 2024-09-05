b393b54d5c3329af056f1fd4d4894a8af8c19db4
sharkocha <shineccy@aliyun.com>
2024-09-05 09:09:28 +0800

    update version

--- src/version.h
+++ src/version.h
@@ -7,11 +7,11 @@
 #define XTATE_NAME             "xtate"
 #define XTATE_NAME_ALL_CAPS    "XTATE"
 #define XTATE_NAME_TITLE_CASE  "Xtate"
-#define XTATE_VERSION          "1.5.1"
+#define XTATE_VERSION          "1.5.2"
 #define XTATE_VERSION_MAJOR    1
 #define XTATE_VERSION_MINOR    5
-#define XTATE_VERSION_PATCH    1
-#define XTATE_WITH_VERSION     "xtate/1.5.1"
+#define XTATE_VERSION_PATCH    2
+#define XTATE_WITH_VERSION     "xtate/1.5.2"
 #define XTATE_DESCRIPTION      "A modular all-stack network scanner for next-generation internet surveys."
 #define XTATE_AUTHOR           "Sharkocha"
 #define XTATE_CONTACT          "chenchiyu14@nudt.edu.cn"
